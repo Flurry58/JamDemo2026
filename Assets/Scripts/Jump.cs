cf40007f9d2bb19d2a2a5a947987382c41823864
LABSTUD24 LABSTUD24 <labstud24@dis.dk>
2026-09-07 14:37:57 +0200

    Final Conversion

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+    public void JumpUp()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
